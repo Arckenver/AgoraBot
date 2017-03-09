55082ca9be16990fcc9020b43b921372ac2287cf
Arckenver <arckenver@gmail.com>
2017-03-09 15:48:07 +0100

    JAC

--- web/app/js/client.js
+++ web/app/js/client.js
@@ -4,8 +4,26 @@
 
 export default class Client
 {
-	constructor()
+	constructor(addr)
 	{
-		
+		this.socket = new WebSocket(addr)
+		this.socket.onopen = this.onConnected.bind(this)
+		this.socket.onmessage = this.onMessage.bind(this)
+		this.socket.onclose = this.onClose.bind(this)
+	}
+
+	onMessage(e)
+	{
+
+	}
+
+	onConnected(e)
+	{
+
+	}
+
+	onClose(e)
+	{
+
 	}
 }
