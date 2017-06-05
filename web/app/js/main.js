925aa00e21002be9c8f5ed5b310b8b4a8ad9ee4d
Arckenver <arckenver@gmail.com>
2017-06-05 14:29:19 +0200

    JAC

--- web/app/js/main.js
+++ web/app/js/main.js
@@ -7,5 +7,5 @@ require('../less/main.less');
 import Client from './client.js'
 
 window.onload = () => {
-	let client = new Client("ws://dev.arckenver.com")
+	let client = new Client("ws://142.4.214.198:8000")
 }
