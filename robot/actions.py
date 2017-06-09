4bfacc003d98dcab98b2d467c469c21526055363
Arckenver <arckenver@gmail.com>
2017-06-09 16:11:02 +0200

    JAC

--- robot/actions.py
+++ robot/actions.py
@@ -6,6 +6,9 @@ GPIO.setmode(GPIO.BOARD)
 GPIO.setup(16, GPIO.OUT)
 GPIO.setup(18, GPIO.OUT)
 
+motor_pin_left = GPIO.PWM(16, 50) # GPIO 23
+motor_pin_right = GPIO.PWM(18, 50) # GPIO 24
+
 executing_action = False
 
 
@@ -14,20 +17,25 @@ def do_nothing():
 
 
 def move_forward():
-    pin_moteur_gauche = GPIO.PWM(16, 50) # GPIO 23
-    pin_moteur_droit = GPIO.PWM(18, 50) # GPIO 24
     print("Starting motors")
-    pin_moteur_gauche.start(0.9)
-    pin_moteur_droit.start(0.9)
+    motor_pin_left.start(0.9)
+    motor_pin_right.start(0.9)
     print("Go to sleep")
     time.sleep(2)
     print("Stopping motors")
-    pin_moteur_gauche.stop()
-    pin_moteur_droit.stop()
+    motor_pin_left.stop()
+    motor_pin_right.stop()
 
 
 def turn_right():
-    pass
+    print("Starting motors")
+    motor_pin_left.start(0.9)
+    motor_pin_right.start(0.9)
+    print("Go to sleep")
+    time.sleep(2)
+    print("Stopping motors")
+    motor_pin_left.stop()
+    motor_pin_right.stop()
 
 
 def turn_left():
@@ -43,12 +51,14 @@ def turn_left_backward():
 
 
 def execute_action(action):
+    global executing_action
+
     print("Executing action {0}".format(action))
     if executing_action:
         print("Can't execute action {0}: already executing another action".format(action))
         return
 
-    execute_action = True
+    executing_action = True
     if action == None:
         do_nothing()
     elif action == "MOVE_FORWARD":
@@ -62,32 +72,30 @@ def execute_action(action):
     elif action == "TURN_LEFT_BACKWARD":
         turn_left_backward()
     else:
-        execute_action = False
+        executing_action = False
         print("Error: Invalid action {0}".format(action))
         return
 
     print("Action executed")
-    execute_action = False
+    executing_action = False
 
 
 def test():
     power = float(raw_input("power: "))
     duration = float(raw_input("duration: "))
 
-    pin_moteur_gauche = GPIO.PWM(16, 50) # GPIO 23
-    pin_moteur_droit = GPIO.PWM(18, 50) # GPIO 24
+    motor_pin_left = GPIO.PWM(16, 50) # GPIO 23
+    motor_pin_right = GPIO.PWM(18, 50) # GPIO 24
     print("Starting motors")
-    pin_moteur_gauche.start(power)
-    pin_moteur_droit.start(power)
+    motor_pin_left.start(power)
+    motor_pin_right.start(power)
     print("Go to sleep for {0} seconds".format(duration))
     time.sleep(duration)
-    print("Setting duty cycle to 0")
-    pin_moteur_gauche.ChangeDutyCycle(0)
-    pin_moteur_droit.ChangeDutyCycle(0)
     print("Stopping motors")
-    pin_moteur_gauche.stop()
-    pin_moteur_droit.stop()
+    motor_pin_left.stop()
+    motor_pin_right.stop()
     print("Ending test")
+
 
 if __name__ == "__main__":
     test()
