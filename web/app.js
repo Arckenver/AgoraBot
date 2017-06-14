6a1f8c7770137e201e9ef721436b21597b2a8c39
Arckenver <arckenver@gmail.com>
2017-06-14 19:44:07 +0200

    JAC

--- web/app.js
+++ web/app.js
@@ -3,12 +3,15 @@
  */
 
 var express = require('express');
+var favicon = require('serve-favicon');
+var path = require('path');
 
 var app = express();
 
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
+app.use(favicon(__dirname + '/public/favicon.ico')))
 app.use('/public', express.static(__dirname + '/public'));
 
 app.use('/', (req, res) => {
