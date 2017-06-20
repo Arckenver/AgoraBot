93903385c8b4a7a7f34350873afc43c71b5beb3c
Arckenver <arckenver@gmail.com>
2017-06-20 02:17:20 +0200

    JAC

--- web/app/js/main.js
+++ web/app/js/main.js
@@ -7,5 +7,5 @@ require('../less/main.less');
 import Client from './client.js'
 
 window.onload = () => {
-	let client = new Client("ws://dev.arckenver.com:8000")
+	let client = new Client("ws://agorabot.imageisima.fr:8000")
 }
